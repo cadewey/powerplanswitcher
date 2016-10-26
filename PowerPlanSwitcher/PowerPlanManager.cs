4a8c8776f3f0e8085387a05ce031d3fe55ab5e48
Christian DeTamble <christian.detamble@outlook.com>
2016-10-26 18:32:05 +0200

    Added copyright notices

--- PowerPlanSwitcher/PowerPlanManager.cs
+++ PowerPlanSwitcher/PowerPlanManager.cs
@@ -1,8 +1,29 @@
-﻿using System;
+﻿// Copyright (C) 2016  PowerPlanSwitcher
+// 
+// This file is part of PowerPlanSwitcher.
+// 
+// PowerPlanSwitcher is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// PowerPlanSwitcher is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with PowerPlanSwitcher.  If not, see <http://www.gnu.org/licenses/>.
+
+#region
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
+
+#endregion
 
 namespace TheRefactory
 {
--- PowerPlanSwitcher/PowerPlanSwitcher.cs
+++ PowerPlanSwitcher/PowerPlanSwitcher.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿// Copyright (C) 2016  PowerPlanSwitcher
+// 
+// This file is part of PowerPlanSwitcher.
+// 
+// PowerPlanSwitcher is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// PowerPlanSwitcher is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with PowerPlanSwitcher.  If not, see <http://www.gnu.org/licenses/>.
+
+#region
+
+using System;
 using System.Drawing;
 using System.Reflection;
 using System.Resources;
@@ -6,19 +25,22 @@ using System.Threading;
 using System.Windows.Forms;
 using TheRefactory.Properties;
 
+#endregion
+
 namespace TheRefactory
 {
     public class SysTrayApp : Form
     {
-        private readonly PowerPlanManager _powerPlanManager = new PowerPlanManager();
-        private readonly NotifyIcon _trayIcon;
-        private readonly ContextMenu _trayMenu = new ContextMenu();
-
-        private readonly Icon[] _icons = {
+        private readonly Icon[] _icons =
+        {
             new Icon(Resources.battery, 32, 32),
             new Icon(Resources.battery_red, 32, 32),
             new Icon(Resources.battery_green, 32, 32)
         };
+
+        private readonly PowerPlanManager _powerPlanManager = new PowerPlanManager();
+        private readonly NotifyIcon _trayIcon;
+        private readonly ContextMenu _trayMenu = new ContextMenu();
 
         public SysTrayApp()
         {
